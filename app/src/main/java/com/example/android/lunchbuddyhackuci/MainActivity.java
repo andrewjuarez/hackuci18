ab93f4ee6892d50ab877be5ef3e5551241e38005
Kevin Yin <keviny.whs@gmail.com>
2018-02-03 14:00:16 -0800

    activity_main.xml

--- app/src/main/java/com/example/android/lunchbuddyhackuci/MainActivity.java
+++ app/src/main/java/com/example/android/lunchbuddyhackuci/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.android.lunchbuddyhackuci;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+
     }
 }
