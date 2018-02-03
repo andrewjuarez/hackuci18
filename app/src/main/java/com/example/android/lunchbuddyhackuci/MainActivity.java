6493b7f3815248ed15fe7238af0d9bdd1e36be11
harleyhacker <a777juarez@gmail.com>
2018-02-03 14:24:13 -0800

    Added button that loads the map ui activty

--- app/src/main/java/com/example/android/lunchbuddyhackuci/MainActivity.java
+++ app/src/main/java/com/example/android/lunchbuddyhackuci/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.lunchbuddyhackuci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button nextActivity = (Button) findViewById(R.id.mapButtonActivityLoader);
+    }
+
+    public void openMap(View view){
+        setContentView(R.layout.map_ui);
     }
 }
