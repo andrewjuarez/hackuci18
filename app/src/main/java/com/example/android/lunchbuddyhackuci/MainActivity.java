bc5f8ed05a4f6868e7324abaaa358624d41b0bf0
Kevin Yin <keviny.whs@gmail.com>
2018-02-03 16:05:39 -0800

    NEW

--- app/src/main/java/com/example/android/lunchbuddyhackuci/MainActivity.java
+++ app/src/main/java/com/example/android/lunchbuddyhackuci/MainActivity.java
@@ -3,15 +3,48 @@ package com.example.android.lunchbuddyhackuci;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 public class MainActivity extends AppCompatActivity {
+    private CallbackManager callbackManager;
 
-    @Override
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        callbackManager.onActivityResult(requestCode, resultCode, data);
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
 
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        System.out.print("SUCCESS!");
+
+
+                        setContentView(R.layout.map_ui);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
     }
+
+
 }
